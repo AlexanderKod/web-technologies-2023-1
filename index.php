25872cc28d4f644abb04ec15274b51d71237d3b9
Kodin Alexander <sashamen228@mail.ru>
2024-05-29 14:55:48 +0500

    lesson17

--- index.php
+++ index.php
@@ -1,52 +1,112 @@
-<?php
-    date_default_timezone_set('Asia/Yekaterinburg');
-    $title = 'php-16';
-    $content_h1 = "Заголовок";
+<?
+$a = 1;
+$b = 2;
 
-    function getCorrectFormatTime($current_time): string
-    {
-        $current_hour = $current_time['tm_hour'];
-        $current_min = $current_time['tm_min'];
+echo "<h2>пункт 1</h2>";
+if($a >= 0 and $b >= 0) {
+    echo "Разность a и b: " . ($a - $b);
+}
+else if($a < 0 and $b < 0) {
+    echo "Произведение a и b: " . ($a * $b); 
+}
+else {
+    echo "Сумма a и b: " . ($a + $b);
+}
 
-        if($current_hour>4 && $current_hour < 21){
-            $hour_ps = 'часов';
-        }
-        elseif ($current_hour%10==1){
-            $hour_ps = 'час';
-        }
-        else{
-            $hour_ps = 'часа';
-        }
+echo "<h2>пункт 2</h2>";
+$a = rand(0, 15);
+echo "\n";
 
-        if(($current_min>4 && $current_min < 21) || $current_min%10==0 || $current_min%10>4){
-            $min_ps = 'минут';
-        }
-        elseif ($current_min%10==1){
-            $min_ps = 'минута';
-        }
-        else{
-            $min_ps = 'минуты';
-        }
-
-        return "$current_hour $hour_ps $current_min $min_ps";
-    }
+switch ($a) {
+    case 0:
+        echo nl2br("\n0");
+    case 1:
+        echo nl2br("\n1");
+    case 2:
+        echo nl2br("\n2");
+    case 3:
+        echo nl2br("\n3");
+    case 4:
+        echo nl2br("\n4");
+    case 5:
+        echo nl2br("\n5");
+    case 6:
+        echo nl2br("\n6");
+    case 7:
+        echo nl2br("\n7");
+    case 8:
+        echo nl2br("\n8");
+    case 9:
+        echo nl2br("\n9");
+    case 10:
+        echo nl2br("\n10");
+    case 11:
+        echo nl2br("\n11");
+    case 12:
+        echo nl2br("\n12");
+    case 13:
+        echo nl2br("\n13");
+    case 14:
+        echo nl2br("\n14");
+    case 15:
+        echo nl2br("\n15");
+}
 ?>
-<!doctype html>
+
+<?
+
+function plus($a, $b){
+    return $a + $b;
+}
+
+function minus($a, $b){
+    return $a - $b;
+}
+
+function multiply($a, $b){
+    return $a * $b;
+}
+
+function divide($a, $b){
+    if($b == 0) {
+        throw new Error("Division by zero");
+    }
+    return $a / $b;
+}
+?>
+
+<?
+
+echo "<h2>пункт 3,4</h2>";
+function mathOperation($arg1, $arg2, $operation) {
+    switch($operation){
+        case "plus":
+            return plus($arg1, $arg2);
+        case "minus":
+            return minus($arg1, $arg2);
+        case "multiply":
+            return multiply($arg1, $arg2);
+        case "divide":
+            return divide($arg1, $arg2);
+        default:
+            throw new Error("An unknown operation type");
+    }
+}
+
+echo nl2br("\nСложение 10 и 10: " . mathOperation(10, 10, "plus"));
+echo nl2br("\nВычитание 10 и 10: " . mathOperation(10, 10, "minus"));
+echo nl2br("\nУмножение 10 и 10: " . mathOperation(10, 10, "multiply"));
+echo nl2br("\nДеление 10 и 10: " . mathOperation(10, 10, "divide"));
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="">
-    <title><?php echo $title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <?php
-        $current_time = localtime(time(), true);
-        $result = getCorrectFormatTime($current_time);
-        echo "<h1>$content_h1</h1>";
-        echo "<h2>$result</h2>";
-    ?>
+    
 </body>
 </html>
